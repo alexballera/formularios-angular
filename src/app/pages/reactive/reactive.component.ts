49dd2286850ce0a1832f846d6a5332514735eff7
Alex Ballera <alexballera@gmail.com>
2020-04-14 23:45:45 -0300

    Validaciones sincronas

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -19,9 +19,9 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario() {
     this.form = this.fb.group({
-      nombre  : ['Alexander'],
-      apellido: ['Ballera'],
-      correo  : ['alexballera@gmail.com'],
+      nombre  : ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      correo  : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
     });
   }
 
