a1e43cffb9b958c9ba1e8690a0ad24e7fea7f4e7
Alex Ballera <alexballera@gmail.com>
2020-04-16 23:04:42 -0300

    Validaciones

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -10,7 +11,8 @@ export class ReactiveComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private validadores: ValidadoresService) {
     this.crearFormulario();
     this.cargarDataAlFormulario();
   }
@@ -21,7 +23,7 @@ export class ReactiveComponent implements OnInit {
   crearFormulario() {
     this.form = this.fb.group({
       nombre  : ['', [Validators.required, Validators.minLength(5)]],
-      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', [Validators.required, Validators.minLength(5), this.validadores.noHerrera]],
       correo  : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
